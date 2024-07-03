f5f457caefc04ef6eca6f902f5f5ada473206fe5
premapandian <premapandian6@gmail.com>
2024-07-03 20:06:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(cors({
     // origin: ['http://localhost:3001'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    // credentials: true
 }))
 
 app.use('/books', booksroute)
