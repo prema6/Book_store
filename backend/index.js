7bd4596ade174751631aa70af42f8ad04236a4ee
premapandian <premapandian6@gmail.com>
2024-07-03 19:52:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json());
 app.use(cors(
     {
-        origin: ['https://book-store-two-peach.vercel.app'],
+        origin: ['https://book-store-frontend-red.vercel.app/'],
         // origin: ['http://localhost:3001'],
         methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
         credentials: true
