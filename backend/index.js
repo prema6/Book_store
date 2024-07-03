de96366b8b73afc531a39b8864529661f6a78ed2
premapandian <premapandian6@gmail.com>
2024-07-03 20:04:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,17 +6,15 @@ import booksroute from './routes/booksroute.js'
 import cors from 'cors'
 const app = express()
 app.use(express.json());
-app.use(cors(
-    {
-        origin: ['https://book-store-frontend-red.vercel.app'],
-        // origin: ['http://localhost:3001'],
-        methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-        credentials: true
-    }
-))
+app.use(cors({
+    origin: 'https://book-store-frontend-red.vercel.app',
+    // origin: ['http://localhost:3001'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
 
 app.use('/books', booksroute)
-
 
 mongoose.connect(Mongo).then(
     ()=>{
