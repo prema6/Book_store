1ebc27fbf316f0004e85ef7aba9bd10f158acf15
premapandian <premapandian6@gmail.com>
2024-07-03 20:11:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,21 @@ const cors = require('cors');
 // import cors from 'cors'
 const app = express()
 app.use(express.json());
-app.use(cors({
-    // origin: 'https://book-store-frontend-red.vercel.app',
-    // origin: ['http://localhost:3001'],
-    origin: '*',
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    // credentials: true
-}))
+// app.use(cors({
+//     // origin: 'https://book-store-frontend-red.vercel.app',
+//     // origin: ['http://localhost:3001'],
+//     origin: '*',
+//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     // credentials: true
+// }))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://book-store-frontend-red.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use('/books', booksroute)
 
