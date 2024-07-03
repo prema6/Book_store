e93744e22f081a5323aac20ebfbe933b89bffb74
premapandian <premapandian6@gmail.com>
2024-07-03 19:46:48 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
-export const Mongo = "mongodb+srv://Gnana:Athityan@cluster0.rxazotu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// export const Mongo = "mongodb+srv://Gnana:Athityan@cluster0.rxazotu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+export const Mongo = "mongodb+srv://premapandian6:58LraP62eVc742k0@cluster0.0vzzlng.mongodb.net/books?retryWrites=true&w=majority"
