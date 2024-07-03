964bf4aa9c03901f87d196e5d84257462820ca35
premapandian <premapandian6@gmail.com>
2024-07-03 20:14:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://book-store-frontend-red.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
